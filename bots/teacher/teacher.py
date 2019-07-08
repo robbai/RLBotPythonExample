b5449f4d734655ce83dfca10a284eee7165d485b
Robbie <robbiealternate@gmail.com>
2019-07-08 21:40:18 +0100

    Change ATBA to teacher

--- bots/teacher/teacher.py
+++ bots/teacher/teacher.py
@@ -3,8 +3,8 @@ import math
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
-from util.orientation import Orientation
-from util.vec import Vec3
+from ..util.orientation import Orientation
+from ..util.vec import Vec3
 
 
 class PythonExample(BaseAgent):
