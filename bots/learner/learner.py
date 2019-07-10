b4f864fa576613fba757944cb3792c70ec1ef32c
Robbie <robbiealternate@gmail.com>
2019-07-10 13:22:49 +0100

    Update learner.py

--- bots/learner/learner.py
+++ bots/learner/learner.py
@@ -5,7 +5,7 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from util.collect_data import format_data, format_labels, data_size, label_size
+from util.collect_data import format_data, format_labels, data_size, label_size, from_labels
 from teacher.teacher import Teacher
 from util.dummy_renderer import DummyRenderer
 
@@ -28,20 +28,21 @@ class Learner(BaseAgent):
         # Network
         self.model = tf.keras.Sequential([
         layers.Dense(data_size, activation = 'relu', input_shape = (data_size,)),
-        layers.Dense(data_size, activation = 'relu'),
+        #layers.Dense(data_size, activation = 'relu'),
         layers.Dense(label_size, activation = 'relu')])
         self.model.compile(optimizer = tf.train.AdamOptimizer(0.001),
-                      loss = 'categorical_crossentropy')
+                      loss = 'mse')
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         teacher_output = self.teacher.get_output(packet)
         
-        data = format_data(self.index, packet, self.get_ball_prediction_struct()).tolist()
+        data = format_data(self.index, packet, self.get_ball_prediction_struct()).reshape((1, data_size))
         #print(data)
-        labels = format_labels(teacher_output).tolist()
+        #return self.controller_state
+        labels = format_labels(teacher_output)
 
         output = self.model.predict(data)
-        self.controller_state = from_labels(output)
+        self.controller_state = from_labels(output[0])
 
         #self.train(data, labels)
         
