7feacad89213d7606623c4f678b8b47b8e233f95
Robbie <robbiealternate@gmail.com>
2019-07-10 11:56:43 +0100

    Dummy renderer

--- bots/learner/learner.py
+++ bots/learner/learner.py
@@ -2,13 +2,15 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 from ..utils.collect_data import format_data
+from ..teacher.teacher import Teacher
+from ..utils.dummy_renderer import DummyRenderer
 
 
 class Learner(BaseAgent):
 
     def initialize_agent(self):
-        # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
+        self.teacher = Teacher(self)
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         data = format_data(self.index, packet, self.get_ball_prediction_struct())
---
+++ bots/util/dummy_renderer.py
@@ -0,0 +1,48 @@
+from rlbot.utils.rendering.rendering_manager import RenderingManager
+
+class DummyRenderer(RenderingManager):
+
+    def __init__(self, renderer):
+        self.renderGroup = renderer.renderGroup
+        self.render_state = renderer.render_state
+        self.builder = renderer.builder
+        self.render_list = renderer.render_list
+        self.group_id = renderer.group_id
+        self.bot_index = renderer.bot_index
+        self.bot_team = renderer.bot_team
+        
+    def clear_screen(self, group_id='default'):
+        pass
+
+    def draw_line_2d(self, x1, y1, x2, y2, color):
+        return self
+
+    def draw_polyline_2d(self, vectors, color):
+        return self
+
+    def draw_line_3d(self, vec1, vec2, color):
+        return self
+
+    def draw_polyline_3d(self, vectors, color):
+        return self
+
+    def draw_line_2d_3d(self, x, y, vec, color):
+        return self
+
+    def draw_rect_2d(self, x, y, width, height, filled, color):
+        return self
+
+    def draw_rect_3d(self, vec, width, height, filled, color, centered=False):
+        return self
+
+    def draw_string_2d(self, x, y, scale_x, scale_y, text, color):
+        return self
+
+    def draw_string_3d(self, vec, scale_x, scale_y, text, color):
+        return self
+
+    def begin_rendering(self, group_id='default'):
+        pass
+
+    def end_rendering(self):
+        pass
